ab736367e1bd8c84336fa4d57aa9567a47399696
Mateusz Skrzątek <skrzatekmateusz@gmail.com>
2026-03-03 11:38:52 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:hVQBNvTcKixRdqpAmexGAHDiZaVwWsbw@postgres.railway.internal:5432/railway"
 
 engine = create_engine(
     DATABASE_URL,
